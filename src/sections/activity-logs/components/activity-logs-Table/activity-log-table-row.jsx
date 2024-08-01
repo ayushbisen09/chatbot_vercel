0ff3a1bdcacac8533df1dfbdd8d3692855e8fc55
pabbly-ayush-bisen <ayush.bisen@pabbly.com>
2024-08-01 11:55:50 +0530

    Activity log Folder Added in Section

---
+++ src/sections/activity-logs/components/activity-logs-Table/activity-log-table-row.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+
+import { Label } from 'src/components/label';
+
+export function ActivitylogTableRow({ row, selected, onViewRow, onSelectRow, onDeleteRow }) {
+  const [setShowToken] = useState(false);
+
+  const renderPrimary = (
+    <TableRow hover selected={selected}>
+      <TableCell width={350}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Stack
+            sx={{
+              typography: 'body2',
+              flex: '1 1 auto',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Box component="span">Jan 19, 2024</Box>
+            <Box component="span" sx={{ color: 'text.disabled' }}>
+              08:23:31
+            </Box>
+          </Stack>
+        </Stack>
+      </TableCell>
+
+      <TableCell width={700}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Stack
+            sx={{
+              typography: 'body2',
+              flex: '1 1 auto',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Box component="span">Ayush Bisen</Box>
+            <Box component="span" sx={{ color: 'text.disabled' }}>
+              ayush.bisen@pabbly.com
+            </Box>
+          </Stack>
+        </Stack>
+      </TableCell>
+      <TableCell width={700}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Stack
+            sx={{
+              typography: 'body2',
+              flex: '1 1 auto',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Box component="span">Team Member Login</Box>
+            <Box component="span" sx={{ color: 'text.disabled' }}>
+              User
+            </Box>
+          </Stack>
+        </Stack>
+      </TableCell>
+
+      <TableCell width={700}>
+        <Label
+          variant="soft"
+          color={
+            (row.status === 'created' && 'success') ||
+            (row.status === 'updated' && 'error') ||
+            'success'
+          }
+        >
+          {row.status}
+        </Label>
+      </TableCell>
+      <TableCell width={592}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Stack
+            sx={{
+              typography: 'body2',
+              flex: '1 1 auto',
+              alignItems: 'flex-start',
+            }}
+          >
+            <Box
+              component="span"
+              sx={{
+                color: '#078DEE',
+                display: 'inline-block',
+                maxWidth: '150px', // Adjust this value as needed
+                whiteSpace: 'nowrap',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+              }}
+            >
+              6671324303c8782026584551846
+            </Box>
+          </Stack>
+        </Stack>
+      </TableCell>
+    </TableRow>
+  );
+
+  return <>{renderPrimary}</>;
+}
