233c21861c348564e95f63427ba19d141ffb7689
pabbly-ayush-bisen <ayush.bisen@pabbly.com>
2024-09-18 13:26:40 +0530

    Added Template preview in Pre-Approved Message

--- src/sections/optIn-management/pre-approved-message.jsx
+++ src/sections/optIn-management/pre-approved-message.jsx
@@ -11,30 +11,24 @@ import {
   TextField,
   Typography,
   InputAdornment,
+  Card,
+  CardHeader,
+  Avatar,
+  IconButton,
 } from '@mui/material';
 
 import { Iconify } from 'src/components/iconify';
 import FileUpload from 'src/components/upload/upload';
 import { usePopover, CustomPopover } from 'src/components/custom-popover';
+import FileType from './hook/messages-type/file';
 
-import ChatBox from './components/chat-box/chat-box';
-import Image from '../../assets/images/chatImage/imagechat.png';
+import AudioType from './hook/messages-type/audio';
+
+import VideoType from './hook/messages-type/video';
 
 export default function PreApprovedMessage() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-
-  const TEMPLATES = [
-    { value: 'template1', label: 'Template 1' },
-    { value: 'template2', label: 'Template 2' },
-    { value: 'template3', label: 'Template 3' },
-    { value: 'template4', label: 'Template 4' },
-  ];
-
-  const [template, setTemplate] = useState('template1');
-  const handleChangeTemplate = useCallback((event) => {
-    setTemplate(event.target.value);
-  }, []);
 
   const [isFileUploaded, setIsFileUploaded] = useState(false);
   const handleFileUpload = (file) => {
@@ -52,6 +46,86 @@ export default function PreApprovedMessage() {
   const [textFieldValue2, setTextFieldValue2] = useState('');
   const [textFieldValue3, setTextFieldValue3] = useState('');
   const [textFieldValue4, setTextFieldValue4] = useState('');
+
+  const [chatBoxImage, setChatBoxImage] = useState(''); // State for the image based on the selected type
+  const [message, setMessage] = useState(
+    <>
+      {`Hi {{1}}! 🎧🛒`}
+      <br />
+      <br />
+      Congratulations! 🎉 Your order for the Headway Bassheads has been confirmed. 🙌
+      <br />
+      <br />
+      Order Details:
+      <br />
+      {`Product: {{2}}`}
+      <br />
+      {`Quantity: {{3}}`}
+      <br />
+      {`Order ID: {{4}}`}
+      <br />
+      {`Delivery Address: {{5}}`}
+      <br />
+      {`Estimated Delivery Date: {{6}}`}
+    </>
+  );
+  const [templateType, setmessagetype] = useState('text');
+  const handleChangemessagetype = useCallback((event) => {
+    const selectedType = event.target.value;
+    setmessagetype(selectedType);
+    if (selectedType === 'file' || selectedType === 'audio' || selectedType === 'video') {
+      setMessage(
+        <>
+          {`Hi {{1}}! 🎧🛒`}
+          <br />
+          <br />
+          Congratulations! 🎉 Your order for the Headway Bassheads has been confirmed. 🙌
+          <br />
+          <br />
+          Order Details:
+          <br />
+          {`Product: {{2}}`}
+          <br />
+          {`Quantity: {{3}}`}
+          <br />
+          {`Order ID: {{4}}`}
+          <br />
+          {`Delivery Address: {{5}}`}
+          <br />
+          {`Estimated Delivery Date: {{6}}`}
+        </>
+      );
+    }
+
+    // Update the chat box image based on the selected type
+    switch (selectedType) {
+      case 'text':
+        setChatBoxImage('');
+        break;
+      case 'image':
+        setChatBoxImage('../../assets/images/chatImage/imagechat.png');
+        break;
+      case 'video':
+        setChatBoxImage('');
+        break;
+      case 'file':
+        setChatBoxImage('');
+        break;
+      case 'audio':
+        setChatBoxImage('');
+        break;
+      default:
+        setChatBoxImage('../../assets/images/chatImage/default.png');
+    }
+  }, []);
+
+  const TEMPLATETYPE = [
+    { value: 'text', label: 'Text Template' },
+    { value: 'image', label: 'Image Template' },
+    { value: 'file', label: 'File Template' },
+    { value: 'video', label: 'Video Template' },
+    { value: 'audio', label: 'Audio Template' },
+  ];
 
   const handlePopoverClose = (popover) => popover.onClose();
   const handlePopoverOpen = (popover) => popover.onOpen();
@@ -84,23 +158,23 @@ export default function PreApprovedMessage() {
     <Box sx={{ mt: '24px' }}>
       <Box display="flex" flexDirection={isMobile ? 'column' : 'row'} width="100%">
         <Box width={isMobile ? '100%' : '60%'} pr={isMobile ? 0 : '12px'}>
-        <Tooltip title="Click here to select WhatsApp template." arrow placement="top">
-          <TextField
-            sx={{ mb: '24px' }}
-            id="select-currency-label-x"
-            select
-            fullWidth
-            label="Select WhatsApp Template"
-            value={template}
-            onChange={handleChangeTemplate}
-            helperText="Select one from your WhatsApp approved template messages"
-          >
-            {TEMPLATES.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+          <Tooltip title="Click here to select WhatsApp template." arrow placement="top">
+            <TextField
+              sx={{ mb: 3 }}
+              id="select-messagetype-label-x"
+              select
+              fullWidth
+              label="Select WhatsApp Template"
+              value={templateType}
+              onChange={handleChangemessagetype}
+              helperText="Select one from your WhatsApp approved template messages"
+            >
+              {TEMPLATETYPE.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
           </Tooltip>
           <Divider sx={{ borderStyle: 'dashed' }} />
           <TextField
@@ -168,11 +242,15 @@ export default function PreApprovedMessage() {
               label={`Body Field ${index} (Eg: Ankit)`}
               helperText="This template field is required. If you leave template field as empty, your message may not be delivered."
               value={
-                index === 1 ? textFieldValue1 :
-                index === 2 ? textFieldValue2 :
-                index === 3 ? textFieldValue3 :
-                index === 4 ? textFieldValue4 :
-                ''
+                index === 1
+                  ? textFieldValue1
+                  : index === 2
+                    ? textFieldValue2
+                    : index === 3
+                      ? textFieldValue3
+                      : index === 4
+                        ? textFieldValue4
+                        : ''
               }
               onChange={(e) => {
                 if (index === 1) setTextFieldValue1(e.target.value);
@@ -204,11 +282,15 @@ export default function PreApprovedMessage() {
                             : 'grey'
                         }
                         onClick={
-                          index === 1 ? popover1.onOpen :
-                          index === 2 ? popover2.onOpen :
-                          index === 3 ? popover3.onOpen :
-                          index === 4 ? popover4.onOpen :
-                          undefined
+                          index === 1
+                            ? popover1.onOpen
+                            : index === 2
+                              ? popover2.onOpen
+                              : index === 3
+                                ? popover3.onOpen
+                                : index === 4
+                                  ? popover4.onOpen
+                                  : undefined
                         }
                         icon="fluent:calendar-agenda-24-regular"
                         style={{ width: 20, height: 20, cursor: 'pointer' }}
@@ -219,49 +301,177 @@ export default function PreApprovedMessage() {
               }}
             />
           ))}
-
         </Box>
 
         {renderPopover(popover1, setTextFieldValue1)}
         {renderPopover(popover2, setTextFieldValue2)}
         {renderPopover(popover3, setTextFieldValue3)}
         {renderPopover(popover4, setTextFieldValue4)}
-        <Tooltip title="Pre-approved template message preview" arrow placement="top">
 
-        <Box
-          width={isMobile ? '100%' : '40%'}
-          sx={{ pl: isMobile ? 0 : '12px', mt: isMobile ? '24px' : 0 }}
-        >
-          <ChatBox
-            text={
-              <>
-                {`Hi {{1}}! 🎧🛒`}
-                <br />
-                <br />
-                Congratulations! 🎉 Your order for the Headway Bassheads has been confirmed. 🙌
-                <br />
-                <br />
-                Order Details:
-                <br />
-                {`Product: {{2}}`}
-                <br />
-                {`Quantity: {{3}}`}
-                <br />
-                {`Order ID: {{4}}`}
-                <br />
-                {`Delivery Address: {{5}}`}
-                <br />
-                {`Estimated Delivery Date: {{6}}`}
-              </>
-            }
-            showImage
-            coverSrc={Image}
-            showLinks
-            showCoupon
-            showCall
-            showVisit
-          />
-        </Box>
+        <Tooltip title="Pre-approved template message preview" arrow placement="top">
+          <Box
+            width={isMobile ? '100%' : '40%'}
+            sx={{ pl: isMobile ? 0 : '12px', mt: isMobile ? '24px' : 0 }}
+          >
+            <Card
+              sx={{
+                border: '1px solid #919EAB33',
+                width: '100%',
+                maxWidth: '500px',
+              }}
+            >
+              <CardHeader
+                sx={{ mb: 2 }}
+                avatar={<Avatar aria-label="profile picture">MC</Avatar>}
+                title={
+                  <Typography variant="h7" sx={{ fontSize: 14, fontWeight: '700' }}>
+                    Mireya Conner
+                  </Typography>
+                }
+                subheader={
+                  <Typography variant="subtitle2" sx={{ fontSize: 12, fontWeight: '400' }}>
+                    Online
+                  </Typography>
+                }
+              />
+              <Divider />
+              <Typography
+                variant="caption"
+                sx={{
+                  pr: 2,
+                  pt: 3,
+                  display: 'flex',
+                  color: '#919EAB',
+                  justifyContent: 'end',
+                }}
+              >
+                4:02 PM
+              </Typography>
+              <Box
+                sx={{
+                  p: 2,
+                  backgroundColor: '#CCF4FE',
+                  borderRadius: '8px',
+                  m: 2,
+                }}
+              >
+                {templateType === 'video' && (
+                  <VideoType
+                    videoSrc="../../../public/assets/videos/chat-videos/advertisement.mp4"
+                    captionsSrc="../../assets/captions/sample.vtt"
+                  />
+                )}
+
+                {templateType === 'audio' && (
+                  <AudioType audioSrc="../../../public/assets/audios/new-instrumental.mp3" />
+                )}
+
+                {templateType === 'file' && <FileType />}
+
+                <Box sx={{ mb: 2 }}>
+                  {chatBoxImage && (
+                    <img
+                      src={chatBoxImage}
+                      alt="Chat Preview"
+                      style={{ width: '100%', borderRadius: '8px' }}
+                    />
+                  )}
+                </Box>
+                <Typography
+                  variant="body2"
+                  color="text.primary"
+                  sx={{ fontSize: 14, fontWeight: '500', mb: chatBoxImage ? 0 : 0 }}
+                >
+                  {message}
+                </Typography>
+                <Box>
+                  <Divider sx={{ mt: 1, mb: 1 }} />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center', // Center horizontally
+                      alignItems: 'center', // Center vertically
+                    }}
+                  >
+                    <IconButton
+                      size="small"
+                      sx={{
+                        color: '#007BFF', // Change color of the icon button
+                      }}
+                    >
+                      <Iconify width={20} icon="solar:copy-bold" />
+                    </IconButton>
+                    <Typography
+                      sx={{
+                        color: '#007BFF', // Change color of the text
+                        fontSize: '14px', // Set font size to 12
+                        fontWeight: '400', // Set font weight to medium
+                      }}
+                    >
+                      Coupon Code
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <Divider sx={{ mb: 1, mt: 1 }} />
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center', // Center horizontally
+                      alignItems: 'center', // Center vertically
+                    }}
+                  >
+                    <IconButton
+                      size="small"
+                      sx={{
+                        color: '#007BFF', // Change color of the icon button
+                      }}
+                    >
+                      <Iconify width={20} icon="icon-park-outline:share" />
+                    </IconButton>
+                    <Typography
+                      sx={{
+                        color: '#007BFF', // Change color of the text
+                        fontSize: '14px', // Set font size to 12
+                        fontWeight: '400', // Set font weight to medium
+                      }}
+                    >
+                      Visit Now
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <Divider sx={{ mb: 1, mt: 1 }} />
+
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center', // Center horizontally
+                      alignItems: 'center', // Center vertically
+                    }}
+                  >
+                    <IconButton
+                      size="small"
+                      sx={{
+                        color: '#007BFF', // Change color of the icon button
+                      }}
+                    >
+                      <Iconify width={20} icon="material-symbols:call" />
+                    </IconButton>
+                    <Typography
+                      sx={{
+                        color: '#007BFF', // Change color of the text
+                        fontSize: '14px', // Set font size to 12
+                        fontWeight: '400', // Set font weight to medium
+                      }}
+                    >
+                      Call Now
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Card>
+          </Box>
         </Tooltip>
       </Box>
     </Box>
