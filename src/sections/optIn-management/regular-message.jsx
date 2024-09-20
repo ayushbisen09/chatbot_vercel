576ab21df1bbba0f784adb2b8e37eb8838861807
pabbly-ayush-bisen <ayush.bisen@pabbly.com>
2024-09-20 13:10:33 +0530

    Added Delete icon in the Upload section in opt-in management

--- src/sections/optIn-management/regular-message.jsx
+++ src/sections/optIn-management/regular-message.jsx
@@ -210,7 +210,7 @@ export default function RegularMessage() {
                     {/* Reset the component */}
                   </Box>
                   <Box sx={{ pl: 2 }}>
-                    <Tooltip title="Click here to delete attribute" arrow placement="top">
+                    <Tooltip title="Click here to remove uploaded file" arrow placement="top">
                       <Button
                         size="small"
                         sx={{ color: 'grey.600', minWidth: 'auto' }}
